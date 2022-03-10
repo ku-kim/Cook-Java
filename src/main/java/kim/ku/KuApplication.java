8a07d4efa845e15f11b07c202b22f21f3957dfb6
“kukim” <kukim.dev@gmail.com>
2022-03-11 08:53:18 +0900

    init: project
    - Spring Boot
    - Spring Web
    - Lombok
    - Spring Boot DevTools
    - Thymeleaf
    - Spring Data JPA
    - Mustache
    - H2Database

---
+++ src/main/java/kim/ku/KuApplication.java
@@ -0,0 +1,13 @@
+package kim.ku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kim/ku/KuApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.ku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
